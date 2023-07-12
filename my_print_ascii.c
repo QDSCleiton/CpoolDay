45e72f23550508944d9d0dd12c831fd9836e896b
QDSCleiton <139328482+QDSCleiton@users.noreply.github.com>
2023-07-12 11:59:19 +0000

    main

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -1,20 +1,25 @@
-void print_char(char c)
-{
-       write(1, &c, 1);
-}
+void print_char(char c);
 
 int main(void)
+
 {
-  int i, j, k = 0;
- 
-  my_print_ascii("\n ___ TABLEAU DECIMAL ___");
-  my_print_ascii("\n     0 1 2 3 4 5 6 7 8 9");
-  for(i = 0; i < 26; i++) {
-    print_char("\n %2d  ", i);
-    for(j = 0; j < 10; j++, k++)
-      print_char("%c ", k != 7 && k != 8 && k != 9 && k != 10 && k != 13 && k < 256 ? k : ' ');
+
+  int i;
+
+  {
+
+   for (i = 0; i <= 127; i++)
+   
+     {
+
+      printf('«%3d: %c\n », i, i');
+  
+     } 
+  
   }
  
-  my_print_ascii("\n _______________________");
+  my_print_ascii();
+  
   return 0;
+
 }
