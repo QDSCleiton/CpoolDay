a8f4442e124865391bd3a0cb14b77c078c2526f4
QDSCleiton <139328482+QDSCleiton@users.noreply.github.com>
2023-07-12 09:44:17 +0000

    add print char

---
+++ my_print_ascii.c
@@ -0,0 +1,20 @@
+void print_char(char c)
+{
+       write(1, &c, 1);
+}
+
+int main(void)
+{
+  int i, j, k = 0;
+ 
+   my_print_ascii("\n ___ TABLEAU DECIMAL ___");
+   my_print_ascii("\n     0 1 2 3 4 5 6 7 8 9");
+  for(i = 0; i < 26; i++) {
+    printf("\n %2d  ", i);
+    for(j = 0; j < 10; j++, k++)
+      printf("%c ", k != 7 && k != 8 && k != 9 && k != 10 && k != 13 && k < 256 ? k : ' ');
+  }
+ 
+  my_print_ascii("\n _______________________");
+  return 0;
+}
