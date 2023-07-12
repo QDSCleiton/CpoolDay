af647a199939ee2bd5c1235b21e61528278dd964
QDSCleiton <139328482+QDSCleiton@users.noreply.github.com>
2023-07-12 09:51:52 +0000

    add print correct

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -7,12 +7,12 @@ int main(void)
 {
   int i, j, k = 0;
  
-   my_print_ascii("\n ___ TABLEAU DECIMAL ___");
-   my_print_ascii("\n     0 1 2 3 4 5 6 7 8 9");
+  my_print_ascii("\n ___ TABLEAU DECIMAL ___");
+  my_print_ascii("\n     0 1 2 3 4 5 6 7 8 9");
   for(i = 0; i < 26; i++) {
-    printf("\n %2d  ", i);
+    print_char("\n %2d  ", i);
     for(j = 0; j < 10; j++, k++)
-      printf("%c ", k != 7 && k != 8 && k != 9 && k != 10 && k != 13 && k < 256 ? k : ' ');
+      print_char("%c ", k != 7 && k != 8 && k != 9 && k != 10 && k != 13 && k < 256 ? k : ' ');
   }
  
   my_print_ascii("\n _______________________");
